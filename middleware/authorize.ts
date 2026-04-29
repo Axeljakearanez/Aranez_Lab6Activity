6b16abf6add75f6476b608d46ef4d00ca7bd5043
Aeron Joshua Arañez <jakeaxel911@gmail.com>
2026-04-29 21:19:39 +0000

    Replace project with Node TS MySQL API

---
+++ middleware/authorize.ts
@@ -0,0 +1,38 @@
+import { expressjwt as jwt } from 'express-jwt';
+import config from '../config.json';
+import db from '../helpers/db';
+
+const { secret } = config;
+
+export default function authorize(roles: any = []) {
+  if (typeof roles === 'string') {
+    roles = [roles];
+  }
+
+  return [
+    jwt({ secret, algorithms: ['HS256'] }),
+
+    async (req: any, res: any, next: any) => {
+      const user = req.auth || req.user;
+
+      if (!user || !user.id) {
+        return res.status(401).json({ message: 'Unauthorized' });
+      }
+
+      const account = await db.Account.findByPk(user.id);
+
+      if (!account || (roles.length && !roles.includes(account.role))) {
+        return res.status(401).json({ message: 'Unauthorized' });
+      }
+
+      req.user = user;
+      req.user.role = account.role;
+
+      const refreshTokens = await account.getRefreshTokens();
+      req.user.ownsToken = (token: any) =>
+        !!refreshTokens.find((x: any) => x.token === token);
+
+      next();
+    }
+  ];
+}
